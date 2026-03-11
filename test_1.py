4f72eeb6ca758cf6915c8fbbea995d94bdabb065
hsj48706310-arch <hsj48706310@gmail.com>
2026-03-11 21:01:02 +0900

    내용 추가하기

---
+++ test_1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="스마트 홈 대시보드 - 홍서지",
+    layout="wide"
+)
+
+st.header("스트림릿 배포 테스트중")
+st.write("스트림릿 배포해보기 - 홍서지")
+
+st.markdown("**징글징글하다 징글징글한 뻔뻔하다 뻔뻔한**")
+st.markdown("*울퉁불퉁하다 울퉁불퉁한*")
